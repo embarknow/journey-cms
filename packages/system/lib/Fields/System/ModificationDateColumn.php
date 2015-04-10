e9856f5a882b1de65775e6754d40b43c56a34b99
Rowan Lewis <rl@nbsp.io>
2015-04-10 11:33:22 +0200

    Added publish index column sizing, sort order is stored in the session, and added filtering to restrict entries to the correct schema.

---
+++ packages/system/lib/Fields/System/ModificationDateColumn.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Embark\CMS\Fields\System;
+
+use Embark\CMS\Entries\EntryInterface;
+use Embark\CMS\Fields\FieldColumnInterface;
+use Embark\CMS\Fields\FieldColumnTrait;
+use Embark\CMS\Schemas\SchemaInterface;
+use Embark\CMS\SystemDateTime;
+use DOMElement;
+use Widget;
+
+class ModificationDateColumn implements FieldColumnInterface
+{
+    use FieldColumnTrait;
+
+    public function appendBodyElement(DOMElement $wrapper, SchemaInterface $schema, EntryInterface $entry, $url)
+    {
+        $document = $wrapper->ownerDocument;
+        $date = new SystemDateTime($entry->modification_date);
+        $date = $date->toUserDateTime();
+        $body = $document->createElement('td');
+        $wrapper->appendChild($body);
+
+        if ($this['editLink']) {
+            $link = Widget::Anchor(
+                $date->format(__SYM_DATETIME_FORMAT__),
+                $url . '/edit/' . $entry->entry_id
+            );
+            $body->appendChild($link);
+        }
+
+        else {
+            $body->setValue($date->format(__SYM_DATETIME_FORMAT__));
+        }
+    }
+}

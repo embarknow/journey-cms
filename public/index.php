08b6b9b96a5b3ddc1a22fe9f312f9cfb008471be
John Porter <john@designermonkey.co.uk>
2015-04-23 23:21:28 +0100

    Slimmed down index file

--- public/index.php
+++ public/index.php
@@ -3,86 +3,18 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Pimple\Container;
-use Phly\Http\Server;
-use Embark\Journey\Exceptions\ErrorHandler;
-use Embark\Journey\Exceptions\NativeExceptionHandler;
-use Embark\Journey\Exceptions\XMLExceptionHandler;
-use Embark\Journey\MiddlewareStack;
-use Embark\Journey\Middleware\Bootstrap;
+
+use Embark\Journey\Middleware\Application;
 
 define('DOCROOT', rtrim(dirname(__FILE__), '\\/public'));
 define('DOMAIN', rtrim(rtrim($_SERVER['HTTP_HOST'], '\\/') . dirname($_SERVER['PHP_SELF']), '\\/'));
 
 $container = new Container;
-
-/**
- * The middleware stack allows components to be added to the application in a layered fashion. These middleware are allowed to affect the running and outcome of the application. For example, authentication can prevent the application from running in full.
- */
-$container['middleware'] = function ($con) {
-    return new MiddlewareStack;
-};
-
-/**
- * The error handler takes any application exception and handles it gracefully
- */
-$container['error-handler'] = function ($con) {
-    // return new ErrorHandler(
-    //     __DIR__ . '/../lib/templates',
-    //     new NativeExceptionHandler,
-    //     new XMLExceptionHandler
-    // );
-};
-
-/**
- * The server serves the application. Simples.
- */
-$container['server'] = function ($con) {
-    $middleware = $con['middleware'];
-
-    return Server::createServer(
-        $middleware,
-        $_SERVER,
-        $_GET,
-        $_POST,
-        $_COOKIE,
-        $_FILES
-    );
-};
-
-/**
- * Add Middleware
- */
-
-// Quick and dirty middleware to output stuff while building
-$container['middleware']->addMiddleware(
-    function ($request, $response, $next = null) use ($container) {
-        // var_dump($container['server']->request->getUri()->getPath());
-        $router = $container['router'];
-        $router($request, $response);
-        // var_dump($container['router']);
-        // var_dump((new Embark\Journey\Routes\Controller)->read('route-groups'));
-
-        return (
-            $next
-            ? $next($request, $response)
-            : $response
-        );
-    }
-);
-
-// The router is set up based on the request object
-// $container['middleware']->addMiddleware(
-//     $container['router']
-// );
-
-/**
- * Bootstrap must be the last middleware added, so it is the first called off the top of the stack
- */
-$container['middleware']->addMiddleware(
-    new Bootstrap($container)
-);
+$application = new Application($container);
 
 /**
  * Run the application, providing the error handler for fallback
  */
-$container['server']->listen($container['error-handler']);
+$container['server']->listen(
+    $container['error-handler']
+);
